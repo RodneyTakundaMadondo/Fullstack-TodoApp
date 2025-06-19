caf3c0fa96020a7b058ec6b5d11bc45afa64bf8f
Rodney Madondo <rodney@finfind.co.za>
2025-06-19 13:50:58 +0200

    added db stuff and fixed appsettings

--- To do List App/Program.cs
+++ To do List App/Program.cs
@@ -54,4 +54,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<TaskDbContext>();
+    db.Database.Migrate();
+}
+
+
 app.Run();
